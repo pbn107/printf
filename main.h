60937524b82508c461095254732e577c7bbdd72e
Antexman <anteneh.a@hotmail.com>
2023-01-21 15:53:38 +0000

    file change main.h

--- main.h
+++ main.h
@@ -1,13 +1,11 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
 #include <unistd.h>
-
-
 
 /**
  * struct format - match the conversion specifiers for printf
